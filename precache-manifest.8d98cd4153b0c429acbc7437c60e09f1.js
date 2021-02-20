8695cc1e12793c355e2a75e34f5b1000e1b9f48b
dennerpl <denner.souzapl@gmail.com>
2021-02-20 08:47:22 -0300

    Updates

---
+++ precache-manifest.8d98cd4153b0c429acbc7437c60e09f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54826ba51612515c5e33983662d765c",
+    "url": "/starwars-datatable-hooks/index.html"
+  },
+  {
+    "revision": "b62e848847d2b5ba72ff",
+    "url": "/starwars-datatable-hooks/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "e1dd35ff5b6d32e06444",
+    "url": "/starwars-datatable-hooks/static/js/2.4386ee75.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-datatable-hooks/static/js/2.4386ee75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62e848847d2b5ba72ff",
+    "url": "/starwars-datatable-hooks/static/js/main.23869dc0.chunk.js"
+  },
+  {
+    "revision": "0078f54fea95fa1ab8c2",
+    "url": "/starwars-datatable-hooks/static/js/runtime-main.5be44aa5.js"
+  }
+]);
